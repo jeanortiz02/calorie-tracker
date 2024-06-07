32f301db70ba8d5a0cff0a063dc45aa80ee2a94c
Jean Carlos Ortiz <105516848+jeanortiz02@users.noreply.github.com>
2024-06-07 11:06:35 -0400

    10 videos

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,33 +1,63 @@
-import { useState, ChangeEvent } from 'react'
+import { useState, ChangeEvent, FormEvent, Dispatch } from 'react'
+
+import { v4 as uuidv4 } from 'uuid';
+
 import { categories } from '../data/categories'
 import { Activity } from '../types';
+import { ActivityActions } from '../reducers/activity-reducer';
 
 
-export default function Form() {
+type formProps = {
+    dispatch : Dispatch<ActivityActions>
+}
 
-    const [activity, setActivity] = useState<Activity>({
-        category: 1,
-        name: '',
-        calories: 0,
-    })
+const initialState : Activity = {
+    id: uuidv4(),
+    category: 1,
+    name: '',
+    calories: 0,
+}
+
+export default function Form({dispatch} : formProps) {
+
+    const [activity, setActivity] = useState<Activity>(initialState)
 
     const handlechange = (e : ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>) => {
-        
         const isFieldNumber : boolean = ['calories', 'category'].includes(e.target.id);
-
-        console.log(isFieldNumber)
-
+        // console.log(isFieldNumber)
         setActivity({
             ...activity,
             [e.target.id] : isFieldNumber? +e.target.value : e.target.value
         })
         // console.log(e.target.id);
         // console.log(e.target.value);
+
+        
     };
+
+    const isValidActivity = () => {
+        const { name, calories } = activity;
+        // console.log(name.trim() !== '' && calories > 0);
+        return name.trim() !== '' && calories > 0
+
+    }
+
+    const handleSubmit = (e : FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        dispatch({ type: 'save-activity', payload: {newActivity: activity} })
+
+        // Limpiar el formulario y llamar un nuevo id
+        setActivity({
+            ...initialState,
+            id: uuidv4(),
+        })
+
+    }
 
   return (
     <form 
         className=" space-y-5 bg-white shadow p-10 rounded-lg"
+        onSubmit={handleSubmit}
     >
         <div className="grid grid-cols-1 gap-3">
             <label htmlFor="category" className='font-bold'>Categoría</label>
@@ -76,8 +106,9 @@ export default function Form() {
 
         <input 
             type="submit" 
-            className='bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase cursor-pointer text-white'
-            value='Guardar comida o ejercicio'
+            className='bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase cursor-pointer text-white disabled:opacity-10'
+            value={activity.category === 1 ? 'Guardar comida' : 'Guardar ejercicio'}
+            disabled={!isValidActivity()}
         />
     </form>
   )
